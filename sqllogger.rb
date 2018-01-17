a635d83fdf80580c936da49c57f8668c4d994aba
Trond Albinussen <trond.albinussen@gmail.com>
2018-01-17 12:42:04 +0100

    Typo

--- sqllogger.rb
+++ sqllogger.rb
@@ -1,4 +1,4 @@
-class SqlLogger < Formula
+class Sqllogger < Formula
   desc "A command line tool to filter mysql logs"
   url "https://github.com/trondal/sqllogger/blob/master/archive/sqllogger"
   sha256 "bb3764de3a701a748a40f829752f00dbf2ff457fa65c7d2321c77837df66803d"
